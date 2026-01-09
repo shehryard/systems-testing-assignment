3aeb14b605b9307a963c2178786bcd0fa29b9531
shehryardurrani <shehryardurrani@hotmail.co.uk>
2026-01-09 00:51:45 +0000

    "T2 - improved roman numeral conversion logic"

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-// Our converter function
+// Coverter function logic
+
 const Roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 const numToRoman = (num) => {
-    if (num >3999) {
-        return 'The Romans had no representation for values above 3999';
+  num = Number(num);
+
+  if (!Number.isFinite(num)) return "Please enter a valid number.";
+  if (!Number.isInteger(num)) return "Please enter a whole number (no decimals).";
+  if (num > 3999) return "The Romans had no representation for values above 3999";
+  if (num <= 0) return "The Romans had no representation for 0 and below";
+
+  let result = "";
+
+  for (let i = 0; i < arabic.length; i++) {
+    while (num >= arabic[i]) {
+      result += Roman[i];
+      num -= arabic[i];
     }
-    if (num <=0) {
-        return 'The Romans had no representation for 0 and below';
-    }
-    let result = '';
-    arabic.map((value, index) => {
-        while(num >= value) {
-            result += Roman [index];
-            num -= value;
-        }
-    });
-    return result;
+  }
+  return result;
 };
 
-// Exporting the function to be used in another file
-module.exports = {numToRoman};
+// Works in Jest (Node) AND in Browser
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { numToRoman };
+} else {
+  window.numToRoman = numToRoman;
+}
